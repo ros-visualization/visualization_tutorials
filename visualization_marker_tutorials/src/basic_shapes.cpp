af08300c386ca56f9fd881e0d4916a6e6f0f90a4
Josh Faust <jfaustwg@willowgarage.com>
2010-07-29 23:12:51 +0000

    add newlines back in now that the GetCodeTagged macro supports removing the extra newlines

--- visualization_marker_tutorials/src/basic_shapes.cpp
+++ visualization_marker_tutorials/src/basic_shapes.cpp
@@ -32,6 +32,7 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
 // %EndTag(INCLUDES)%
+
 // %Tag(INIT)%
 int main( int argc, char** argv )
 {
@@ -40,10 +41,12 @@ int main( int argc, char** argv )
   ros::Rate r(1);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
 // %EndTag(INIT)%
+
   // Set our initial shape type to be a cube
 // %Tag(SHAPE_INIT)%
   uint32_t shape = visualization_msgs::Marker::CUBE;
 // %EndTag(SHAPE_INIT)%
+
 // %Tag(MARKER_INIT)%
   while (ros::ok())
   {
@@ -52,20 +55,24 @@ int main( int argc, char** argv )
     marker.header.frame_id = "/my_frame";
     marker.header.stamp = ros::Time::now();
 // %EndTag(MARKER_INIT)%
+
     // Set the namespace and id for this marker.  This serves to create a unique ID
     // Any marker sent with the same namespace and id will overwrite the old one
 // %Tag(NS_ID)%
     marker.ns = "basic_shapes";
     marker.id = 0;
 // %EndTag(NS_ID)%
+
     // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
 // %Tag(TYPE)%
     marker.type = shape;
 // %EndTag(TYPE)%
+
     // Set the marker action.  Options are ADD and DELETE
 // %Tag(ACTION)%
     marker.action = visualization_msgs::Marker::ADD;
 // %EndTag(ACTION)%
+
     // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
 // %Tag(POSE)%
     marker.pose.position.x = 0;
@@ -76,12 +83,14 @@ int main( int argc, char** argv )
     marker.pose.orientation.z = 0.0;
     marker.pose.orientation.w = 1.0;
 // %EndTag(POSE)%
+
     // Set the scale of the marker -- 1x1x1 here means 1m on a side
 // %Tag(SCALE)%
     marker.scale.x = 1.0;
     marker.scale.y = 1.0;
     marker.scale.z = 1.0;
 // %EndTag(SCALE)%
+
     // Set the color -- be sure to set alpha to something non-zero!
 // %Tag(COLOR)%
     marker.color.r = 0.0f;
@@ -89,13 +98,16 @@ int main( int argc, char** argv )
     marker.color.b = 0.0f;
     marker.color.a = 1.0;
 // %EndTag(COLOR)%
+
 // %Tag(LIFETIME)%
     marker.lifetime = ros::Duration();
 // %EndTag(LIFETIME)%
+
     // Publish the marker
 // %Tag(PUBLISH)%
     marker_pub.publish(marker);
 // %EndTag(PUBLISH)%
+
     // Cycle between different shapes
 // %Tag(CYCLE_SHAPES)%
     switch (shape)
@@ -114,6 +126,7 @@ int main( int argc, char** argv )
       break;
     }
 // %EndTag(CYCLE_SHAPES)%
+
 // %Tag(SLEEP_END)%
     r.sleep();
   }
