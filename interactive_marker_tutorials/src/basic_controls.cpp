69e95ad8834c1377ad13d0339656012c1bc058a5
Dave Hershberger <hersh@willowgarage.com>
2011-07-13 20:20:04 +0000

    interactive_marker_tutorials: added mouse-click position feedback printouts to basic_controls.cpp

--- interactive_marker_tutorials/src/basic_controls.cpp
+++ interactive_marker_tutorials/src/basic_controls.cpp
@@ -78,10 +78,19 @@ void processFeedback( const visualization_msgs::InteractiveMarkerFeedbackConstPt
   s << "Feedback from marker '" << feedback->marker_name << "' "
       << " / control '" << feedback->control_name << "'";
 
+  std::ostringstream mouse_point_ss;
+  if( feedback->mouse_point_valid )
+  {
+    mouse_point_ss << " at " << feedback->mouse_point.x
+                   << ", " << feedback->mouse_point.y
+                   << ", " << feedback->mouse_point.z
+                   << " in frame " << feedback->header.frame_id;
+  }
+
   switch ( feedback->event_type )
   {
     case visualization_msgs::InteractiveMarkerFeedback::BUTTON_CLICK:
-      ROS_INFO_STREAM( s.str() << ": button click" );
+      ROS_INFO_STREAM( s.str() << ": button click" << mouse_point_ss.str() << "." );
       break;
 
     case visualization_msgs::InteractiveMarkerFeedback::MENU_SELECT:
@@ -105,11 +114,11 @@ void processFeedback( const visualization_msgs::InteractiveMarkerFeedbackConstPt
       break;
 
     case visualization_msgs::InteractiveMarkerFeedback::MOUSE_DOWN:
-      ROS_INFO_STREAM( s.str() << ": mouse down." );
+      ROS_INFO_STREAM( s.str() << ": mouse down" << mouse_point_ss.str() << "." );
       break;
 
     case visualization_msgs::InteractiveMarkerFeedback::MOUSE_UP:
-      ROS_INFO_STREAM( s.str() << ": mouse up." );
+      ROS_INFO_STREAM( s.str() << ": mouse up" << mouse_point_ss.str() << "." );
       break;
   }
 
