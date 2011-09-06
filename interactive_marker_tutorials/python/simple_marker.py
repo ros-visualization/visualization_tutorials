da6184a1701af9dcdf74e267e1d677379e069fbe
Michael Ferguson <mferguson@willowgarage.com>
2011-09-06 20:13:43 +0000

    Add python examples for interactive_markers

---
+++ interactive_marker_tutorials/python/simple_marker.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2011, Willow Garage, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Willow Garage, Inc. nor the names of its
+      contributors may be used to endorse or promote products derived from
+      this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+"""
+
+import roslib; roslib.load_manifest("interactive_markers")
+import rospy
+
+from interactive_markers.interactive_marker_server import *
+
+def processFeedback(feedback):
+    p = feedback.pose.position
+    print feedback.marker_name + " is now at " + str(p.x) + ", " + str(p.y) + ", " + str(p.z)
+
+if __name__=="__main__":
+    rospy.init_node("simple_marker")
+    
+    # create an interactive marker server on the topic namespace simple_marker
+    server = InteractiveMarkerServer("simple_marker")
+    
+    # create an interactive marker for our server
+    int_marker = InteractiveMarker()
+    int_marker.header.frame_id = "/base_link"
+    int_marker.name = "my_marker"
+    int_marker.description = "Simple 1-DOF Control"
+
+    # create a grey box marker
+    box_marker = Marker()
+    box_marker.type = Marker.CUBE
+    box_marker.scale.x = 0.45
+    box_marker.scale.y = 0.45
+    box_marker.scale.z = 0.45
+    box_marker.color.r = 0.0
+    box_marker.color.g = 0.5
+    box_marker.color.b = 0.5
+    box_marker.color.a = 1.0
+
+    # create a non-interactive control which contains the box
+    box_control = InteractiveMarkerControl()
+    box_control.always_visible = True
+    box_control.markers.append( box_marker )
+
+    # add the control to the interactive marker
+    int_marker.controls.append( box_control )
+
+    # create a control which will move the box
+    # this control does not contain any markers,
+    # which will cause RViz to insert two arrows
+    rotate_control = InteractiveMarkerControl()
+    rotate_control.name = "move_x"
+    rotate_control.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
+
+    # add the control to the interactive marker
+    int_marker.controls.append(rotate_control);
+
+    # add the interactive marker to our collection &
+    # tell the server to call processFeedback() when feedback arrives for it
+    server.insert(int_marker, processFeedback)
+
+    # 'commit' changes and send to all clients
+    server.applyChanges()
+
+    rospy.spin()
+
