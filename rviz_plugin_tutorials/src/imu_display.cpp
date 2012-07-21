a2e9c5de849e13dbc733154381a22a804aee3659
Dave Hershberger <hersh@willowgarage.com>
2012-07-21 00:28:00 +0000

    rviz_plugin_tutorials trunk: ported ImuDisplay, seems to work.

--- rviz_plugin_tutorials/src/imu_display.cpp
+++ rviz_plugin_tutorials/src/imu_display.cpp
@@ -33,8 +33,9 @@
 #include <tf/transform_listener.h>
 
 #include <rviz/visualization_manager.h>
-#include <rviz/properties/property.h>
-#include <rviz/properties/property_manager.h>
+#include <rviz/properties/color_property.h>
+#include <rviz/properties/float_property.h>
+#include <rviz/properties/int_property.h>
 #include <rviz/frame_manager.h>
 
 #include "imu_visual.h"
@@ -48,131 +49,78 @@ namespace rviz_plugin_tutorials
 // The constructor must have no arguments, so we can't give the
 // constructor the parameters it needs to fully initialize.
 ImuDisplay::ImuDisplay()
-  : Display()
-  , scene_node_( NULL )
-  , messages_received_( 0 )
-  , color_( .8, .2, .8 )       // Default color is bright purple.
-  , alpha_( 1.0 )              // Default alpha is completely opaque.
 {
+  color_property_ = new rviz::ColorProperty( "Color", QColor( 204, 51, 204 ),
+                                             "Color to draw the acceleration arrows.",
+                                             this, SLOT( updateColorAndAlpha() ));
+
+  alpha_property_ = new rviz::FloatProperty( "Alpha", 1.0,
+                                             "0 is fully transparent, 1.0 is fully opaque.",
+                                             this, SLOT( updateColorAndAlpha() ));
+
+  history_length_property_ = new rviz::IntProperty( "History Length", 1,
+                                                    "Number of prior measurements to display.",
+                                                    this, SLOT( updateHistoryLength() ));
+  history_length_property_->setMin( 1 );
 }
 
-// After the parent rviz::Display::initialize() does its own setup, it
-// calls the subclass's onInitialize() function.  This is where we
-// instantiate all the workings of the class.
+// After the top-level rviz::Display::initialize() does its own setup,
+// it calls the subclass's onInitialize() function.  This is where we
+// instantiate all the workings of the class.  We make sure to also
+// call our immediate super-class's onInitialize() function, since it
+// does important stuff setting up the message filter.
+//
+//  Note that "MFDClass" is a typedef of
+// ``MessageFilterDisplay<message type>``, to save typing that long
+// templated class name every time you need to refer to the
+// superclass.
 void ImuDisplay::onInitialize()
 {
-  // Make an Ogre::SceneNode to contain all our visuals.
-  scene_node_ = scene_manager_->getRootSceneNode()->createChildSceneNode();
-  
-  // Set the default history length and resize the ``visuals_`` array.
-  setHistoryLength( 1 );
-
-  // A tf::MessageFilter listens to ROS messages and calls our
-  // callback with them when they can be matched up with valid tf
-  // transform data.
-  tf_filter_ =
-    new tf::MessageFilter<sensor_msgs::Imu>( *vis_manager_->getTFClient(),
-                                             "", 100, update_nh_ );
-  tf_filter_->connectInput( sub_ );
-  tf_filter_->registerCallback( boost::bind( &ImuDisplay::incomingMessage,
-                                             this, _1 ));
-
-  // FrameManager has some built-in functions to set the status of a
-  // Display based on callbacks from a tf::MessageFilter.  These work
-  // fine for this simple display.
-  vis_manager_->getFrameManager()
-    ->registerFilterForTransformStatusCheck( tf_filter_, this );
+  MFDClass::onInitialize();
+  updateHistoryLength();
 }
 
 ImuDisplay::~ImuDisplay()
 {
-  unsubscribe();
-  clear();
   for( size_t i = 0; i < visuals_.size(); i++ )
   {
     delete visuals_[ i ];
   }
-
-  delete tf_filter_;
 }
 
 // Clear the visuals by deleting their objects.
-void ImuDisplay::clear()
+void ImuDisplay::reset()
 {
+  MFDClass::reset();
   for( size_t i = 0; i < visuals_.size(); i++ )
   {
     delete visuals_[ i ];
     visuals_[ i ] = NULL;
   }
-  tf_filter_->clear();
-  messages_received_ = 0;
-  setStatus( rviz::status_levels::Warn, "Topic", "No messages received" );
-}
-
-void ImuDisplay::setTopic( const std::string& topic )
-{
-  unsubscribe();
-  clear();
-  topic_ = topic;
-  subscribe();
-
-  // Broadcast the fact that the variable has changed.
-  propertyChanged( topic_property_ );
-
-  // Make sure rviz renders the next time it gets a chance.
-  causeRender();
-}
-
-void ImuDisplay::setColor( const rviz::Color& color )
-{
-  color_ = color;
-
-  propertyChanged( color_property_ );
-  updateColorAndAlpha();
-  causeRender();
-}
-
-void ImuDisplay::setAlpha( float alpha )
-{
-  alpha_ = alpha;
-
-  propertyChanged( alpha_property_ );
-  updateColorAndAlpha();
-  causeRender();
 }
 
 // Set the current color and alpha values for each visual.
 void ImuDisplay::updateColorAndAlpha()
 {
+  float alpha = alpha_property_->getFloat();
+  Ogre::ColourValue color = color_property_->getOgreColor();
+
   for( size_t i = 0; i < visuals_.size(); i++ )
   {
     if( visuals_[ i ] )
     {
-      visuals_[ i ]->setColor( color_.r_, color_.g_, color_.b_, alpha_ );
+      visuals_[ i ]->setColor( color.r, color.g, color.b, alpha );
     }
   }
 }
 
 // Set the number of past visuals to show.
-void ImuDisplay::setHistoryLength( int length )
+void ImuDisplay::updateHistoryLength()
 {
-  // Don't let people enter invalid values.
-  if( length < 1 )
-  {
-    length = 1;
-  }
-  // If the length is not changing, we don't need to do anything.
-  if( history_length_ == length )
-  {
-    return;
-  }
+  int new_length = history_length_property_->getInt();
 
-  // Set the actual variable.
-  history_length_ = length;
-  propertyChanged( history_length_property_ );
-  
   // Create a new array of visual pointers, all NULL.
-  std::vector<ImuVisual*> new_visuals( history_length_, (ImuVisual*)0 );
+  std::vector<ImuVisual*> new_visuals( new_length, (ImuVisual*)0 );
 
   // Copy the contents from the old array to the new.
   // (Number to copy is the minimum of the 2 vector lengths).
@@ -200,144 +148,42 @@ void ImuDisplay::setHistoryLength( int length )
   visuals_.swap( new_visuals );
 }
 
-void ImuDisplay::subscribe()
-{
-  // If we are not actually enabled, don't do it.
-  if ( !isEnabled() )
-  {
-    return;
-  }
-
-  // Try to subscribe to the current topic name (in ``topic_``).  Make
-  // sure to catch exceptions and set the status to a descriptive
-  // error message.
-  try
-  {
-    sub_.subscribe( update_nh_, topic_, 10 );
-    setStatus( rviz::status_levels::Ok, "Topic", "OK" );
-  }
-  catch( ros::Exception& e )
-  {
-    setStatus( rviz::status_levels::Error, "Topic",
-               std::string( "Error subscribing: " ) + e.what() );
-  }
-}
-
-void ImuDisplay::unsubscribe()
-{
-  sub_.unsubscribe();
-}
-
-void ImuDisplay::onEnable()
-{
-  subscribe();
-}
-
-void ImuDisplay::onDisable()
-{
-  unsubscribe();
-  clear();
-}
-
-// When the "Fixed Frame" changes, we need to update our
-// tf::MessageFilter and erase existing visuals.
-void ImuDisplay::fixedFrameChanged()
-{
-  tf_filter_->setTargetFrame( fixed_frame_ );
-  clear();
-}
-
 // This is our callback to handle an incoming message.
-void ImuDisplay::incomingMessage( const sensor_msgs::Imu::ConstPtr& msg )
+void ImuDisplay::processMessage( const sensor_msgs::Imu::ConstPtr& msg )
 {
-  ++messages_received_;
-  
-  // Each display can have multiple status lines.  This one is called
-  // "Topic" and says how many messages have been received in this case.
-  std::stringstream ss;
-  ss << messages_received_ << " messages received";
-  setStatus( rviz::status_levels::Ok, "Topic", ss.str() );
-
   // Here we call the rviz::FrameManager to get the transform from the
   // fixed frame to the frame in the header of this Imu message.  If
   // it fails, we can't do anything else so we return.
   Ogre::Quaternion orientation;
   Ogre::Vector3 position;
-  if( !vis_manager_->getFrameManager()->getTransform( msg->header.frame_id,
-                                                      msg->header.stamp,
-                                                      position, orientation ))
+  if( !context_->getFrameManager()->getTransform( msg->header.frame_id,
+                                                  msg->header.stamp,
+                                                  position, orientation ))
   {
     ROS_DEBUG( "Error transforming from frame '%s' to frame '%s'",
-               msg->header.frame_id.c_str(), fixed_frame_.c_str() );
+               msg->header.frame_id.c_str(), qPrintable( fixed_frame_ ));
     return;
   }
 
+  int history_length = history_length_property_->getInt();
+
   // We are keeping a circular buffer of visual pointers.  This gets
   // the next one, or creates and stores it if it was missing.
-  ImuVisual* visual = visuals_[ messages_received_ % history_length_ ];
+  ImuVisual* visual = visuals_[ messages_received_ % history_length ];
   if( visual == NULL )
   {
-    visual = new ImuVisual( vis_manager_->getSceneManager(), scene_node_ );
-    visuals_[ messages_received_ % history_length_ ] = visual;
+    visual = new ImuVisual( context_->getSceneManager(), scene_node_ );
+    visuals_[ messages_received_ % history_length ] = visual;
   }
 
   // Now set or update the contents of the chosen visual.
   visual->setMessage( msg );
   visual->setFramePosition( position );
   visual->setFrameOrientation( orientation );
-  visual->setColor( color_.r_, color_.g_, color_.b_, alpha_ );
-}
 
-// Override rviz::Display's reset() function to add a call to clear().
-void ImuDisplay::reset()
-{
-  Display::reset();
-  clear();
-}
-
-// Override createProperties() to build and configure a Property
-// object for each user-editable property.  ``property_manager_``,
-// ``property_prefix_``, and ``parent_category_`` are all initialized before
-// this is called.
-void ImuDisplay::createProperties()
-{
-  topic_property_ =
-    property_manager_->createProperty<rviz::ROSTopicStringProperty>( "Topic",
-                                                                     property_prefix_,
-                                                                     boost::bind( &ImuDisplay::getTopic, this ),
-                                                                     boost::bind( &ImuDisplay::setTopic, this, _1 ),
-                                                                     parent_category_,
-                                                                     this );
-  setPropertyHelpText( topic_property_, "sensor_msgs::Imu topic to subscribe to." );
-  rviz::ROSTopicStringPropertyPtr topic_prop = topic_property_.lock();
-  topic_prop->setMessageType( ros::message_traits::datatype<sensor_msgs::Imu>() );
-
-  color_property_ =
-    property_manager_->createProperty<rviz::ColorProperty>( "Color",
-                                                            property_prefix_,
-                                                            boost::bind( &ImuDisplay::getColor, this ),
-                                                            boost::bind( &ImuDisplay::setColor, this, _1 ),
-                                                            parent_category_,
-                                                            this );
-  setPropertyHelpText( color_property_, "Color to draw the acceleration arrows." );
-
-  alpha_property_ =
-    property_manager_->createProperty<rviz::FloatProperty>( "Alpha",
-                                                            property_prefix_,
-                                                            boost::bind( &ImuDisplay::getAlpha, this ),
-                                                            boost::bind( &ImuDisplay::setAlpha, this, _1 ),
-                                                            parent_category_,
-                                                            this );
-  setPropertyHelpText( alpha_property_, "0 is fully transparent, 1.0 is fully opaque." );
-
-  history_length_property_ =
-    property_manager_->createProperty<rviz::IntProperty>( "History Length",
-                                                          property_prefix_,
-                                                          boost::bind( &ImuDisplay::getHistoryLength, this ),
-                                                          boost::bind( &ImuDisplay::setHistoryLength, this, _1 ),
-                                                          parent_category_,
-                                                          this );
-  setPropertyHelpText( history_length_property_, "Number of prior measurements to display." );
+  float alpha = alpha_property_->getFloat();
+  Ogre::ColourValue color = color_property_->getOgreColor();
+  visual->setColor( color.r, color.g, color.b, alpha );
 }
 
 } // end namespace rviz_plugin_tutorials
--- rviz_plugin_tutorials/src/imu_display.h
+++ rviz_plugin_tutorials/src/imu_display.h
@@ -30,14 +30,19 @@
 #ifndef IMU_DISPLAY_H
 #define IMU_DISPLAY_H
 
-#include <message_filters/subscriber.h>
-#include <tf/message_filter.h>
 #include <sensor_msgs/Imu.h>
-#include <rviz/display.h>
+#include <rviz/message_filter_display.h>
 
 namespace Ogre
 {
 class SceneNode;
+}
+
+namespace rviz
+{
+class ColorProperty;
+class FloatProperty;
+class IntProperty;
 }
 
 // All the source in this plugin is in its own namespace.  This is not
@@ -64,32 +69,14 @@ class ImuVisual;
 // themselves are represented by a separate class, ImuVisual.  The
 // idiom for the visuals is that when the objects exist, they appear
 // in the scene, and when they are deleted, they disappear.
-class ImuDisplay: public rviz::Display
+class ImuDisplay: public rviz::MessageFilterDisplay<sensor_msgs::Imu>
 {
+Q_OBJECT
 public:
   // Constructor.  pluginlib::ClassLoader creates instances by calling
   // the default constructor, so make sure you have one.
   ImuDisplay();
   virtual ~ImuDisplay();
-
-  // Overrides of public virtual functions from the Display class.
-  virtual void onInitialize();
-  virtual void fixedFrameChanged();
-  virtual void reset();
-  virtual void createProperties();
-
-  // Setter and getter functions for user-editable properties.
-  void setTopic(const std::string& topic);
-  const std::string& getTopic() { return topic_; }
-
-  void setColor( const rviz::Color& color );
-  const rviz::Color& getColor() { return color_; }
-
-  void setAlpha( float alpha );
-  float getAlpha() { return alpha_; }
-
-  void setHistoryLength( int history_length );
-  int getHistoryLength() const { return history_length_; }
 
   // Overrides of protected virtual functions from Display.  As much
   // as possible, when Displays are not enabled, they should not be
@@ -97,48 +84,29 @@ public:
   // 3D view.  These functions are where these connections are made
   // and broken.
 protected:
-  virtual void onEnable();
-  virtual void onDisable();
+  virtual void onInitialize();
+
+  // A helper to clear this display back to the initial state.
+  virtual void reset();
+
+  // These Qt slots get connected to signals indicating changes in the user-editable properties.
+private Q_SLOTS:
+  void updateColorAndAlpha();
+  void updateHistoryLength();
 
   // Function to handle an incoming ROS message.
 private:
-  void incomingMessage( const sensor_msgs::Imu::ConstPtr& msg );
-
-  // Internal helpers which do the work of subscribing and
-  // unsubscribing from the ROS topic.
-  void subscribe();
-  void unsubscribe();
-
-  // A helper to clear this display back to the initial state.
-  void clear();
-
-  // Helper function to apply color and alpha to all visuals.
-  void updateColorAndAlpha();
+  void processMessage( const sensor_msgs::Imu::ConstPtr& msg );
 
   // Storage for the list of visuals.  This display supports an
   // adjustable history length, so we need one visual per history
   // item.
   std::vector<ImuVisual*> visuals_;
 
-  // A node in the Ogre scene tree to be the parent of all our visuals.
-  Ogre::SceneNode* scene_node_;
-
-  // Data input: Subscriber and tf message filter.
-  message_filters::Subscriber<sensor_msgs::Imu> sub_;
-  tf::MessageFilter<sensor_msgs::Imu>* tf_filter_;
-  int messages_received_;
-
   // User-editable property variables.
-  rviz::Color color_;
-  std::string topic_;
-  float alpha_;
-  int history_length_;
-
-  // Property objects for user-editable properties.
-  rviz::ColorPropertyWPtr color_property_;
-  rviz::ROSTopicStringPropertyWPtr topic_property_;
-  rviz::FloatPropertyWPtr alpha_property_;
-  rviz::IntPropertyWPtr history_length_property_;
+  rviz::ColorProperty* color_property_;
+  rviz::FloatProperty* alpha_property_;
+  rviz::IntProperty* history_length_property_;
 };
 // END_TUTORIAL
 
