a77c906d24dc735eeb44c293298526fef50e0b31
Jacob Perron <jacob@openrobotics.org>
2019-10-15 17:10:04 -0700

    Port Python interactive marker tutorials to ROS 2 (#54)
    * Port Python interactive marker tutorials to ROS 2
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Add missing shutdown call
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Enable Python lint tests and fix errors
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Use f-strings instead of format_map()
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Lint copyright notices
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- interactive_marker_tutorials/scripts/simple_marker.py
+++ interactive_marker_tutorials/scripts/simple_marker.py
@@ -1,54 +1,60 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-"""
-Copyright (c) 2011, Willow Garage, Inc.
-All rights reserved.
+# Copyright (c) 2011, Willow Garage, Inc.
+# All rights reserved.
+#
+# Software License Agreement (BSD License 2.0)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the Willow Garage, Inc. nor the names of its
+#       contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
+import sys
 
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the Willow Garage, Inc. nor the names of its
-      contributors may be used to endorse or promote products derived from
-      this software without specific prior written permission.
+from interactive_markers import InteractiveMarkerServer
+import rclpy
+from visualization_msgs.msg import InteractiveMarker
+from visualization_msgs.msg import InteractiveMarkerControl
+from visualization_msgs.msg import Marker
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-"""
-
-import rospy
-
-from interactive_markers.interactive_marker_server import *
-from visualization_msgs.msg import *
 
 def processFeedback(feedback):
     p = feedback.pose.position
-    print feedback.marker_name + " is now at " + str(p.x) + ", " + str(p.y) + ", " + str(p.z)
+    print(f'{feedback.marker_name} is now at {p.x}, {p.y}, {p.z}')
 
-if __name__=="__main__":
-    rospy.init_node("simple_marker")
-    
-    # create an interactive marker server on the topic namespace simple_marker
-    server = InteractiveMarkerServer("simple_marker")
-    
+
+if __name__ == '__main__':
+    rclpy.init(args=sys.argv)
+    node = rclpy.create_node('simple_marker')
+
+    # create an interactive marker server on the namespace simple_marker
+    server = InteractiveMarkerServer(node, 'simple_marker')
+
     # create an interactive marker for our server
     int_marker = InteractiveMarker()
-    int_marker.header.frame_id = "base_link"
-    int_marker.name = "my_marker"
-    int_marker.description = "Simple 1-DOF Control"
+    int_marker.header.frame_id = 'base_link'
+    int_marker.name = 'my_marker'
+    int_marker.description = 'Simple 1-DOF Control'
 
     # create a grey box marker
     box_marker = Marker()
@@ -64,27 +70,27 @@ if __name__=="__main__":
     # create a non-interactive control which contains the box
     box_control = InteractiveMarkerControl()
     box_control.always_visible = True
-    box_control.markers.append( box_marker )
+    box_control.markers.append(box_marker)
 
     # add the control to the interactive marker
-    int_marker.controls.append( box_control )
+    int_marker.controls.append(box_control)
 
     # create a control which will move the box
     # this control does not contain any markers,
     # which will cause RViz to insert two arrows
     rotate_control = InteractiveMarkerControl()
-    rotate_control.name = "move_x"
+    rotate_control.name = 'move_x'
     rotate_control.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
 
     # add the control to the interactive marker
-    int_marker.controls.append(rotate_control);
+    int_marker.controls.append(rotate_control)
 
     # add the interactive marker to our collection &
     # tell the server to call processFeedback() when feedback arrives for it
-    server.insert(int_marker, processFeedback)
+    server.insert(int_marker, feedback_callback=processFeedback)
 
     # 'commit' changes and send to all clients
     server.applyChanges()
 
-    rospy.spin()
-
+    rclpy.spin(node)
+    server.shutdown()
