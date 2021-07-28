8e5682d996f986d34a8cef05c28984180c8475a0
Rebecca Butler <rebecca@openrobotics.org>
2021-07-28 11:55:19 -0400

    Update basic_shapes and points_and_lines tutorials
    Signed-off-by: Rebecca Butler <rebecca@openrobotics.org>

--- visualization_marker_tutorials/src/basic_shapes.cpp
+++ visualization_marker_tutorials/src/basic_shapes.cpp
@@ -27,54 +27,41 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-// %Tag(FULLTEXT)%
-// %Tag(INCLUDES)%
-#include <ros/ros.h>
-#include <visualization_msgs/Marker.h>
-// %EndTag(INCLUDES)%
+#include <memory>
 
-// %Tag(INIT)%
-int main( int argc, char** argv )
+#include "rclcpp/logging.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+
+int main(int argc, char ** argv)
 {
-  ros::init(argc, argv, "basic_shapes");
-  ros::NodeHandle n;
-  ros::Rate r(1);
-  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-// %EndTag(INIT)%
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("basic_shapes");
+  auto marker_pub = node->create_publisher<visualization_msgs::msg::Marker>("visualization_marker", 1);
+  rclcpp::Rate loop_rate(1);
 
   // Set our initial shape type to be a cube
-// %Tag(SHAPE_INIT)%
-  uint32_t shape = visualization_msgs::Marker::CUBE;
-// %EndTag(SHAPE_INIT)%
+  uint32_t shape = visualization_msgs::msg::Marker::CUBE;
 
-// %Tag(MARKER_INIT)%
-  while (ros::ok())
+  while (rclcpp::ok())
   {
-    visualization_msgs::Marker marker;
+    visualization_msgs::msg::Marker marker;
     // Set the frame ID and timestamp.  See the TF tutorials for information on these.
     marker.header.frame_id = "/my_frame";
-    marker.header.stamp = ros::Time::now();
-// %EndTag(MARKER_INIT)%
+    marker.header.stamp = rclcpp::Clock().now();
 
     // Set the namespace and id for this marker.  This serves to create a unique ID
     // Any marker sent with the same namespace and id will overwrite the old one
-// %Tag(NS_ID)%
     marker.ns = "basic_shapes";
     marker.id = 0;
-// %EndTag(NS_ID)%
 
     // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-// %Tag(TYPE)%
     marker.type = shape;
-// %EndTag(TYPE)%
 
-    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-// %Tag(ACTION)%
-    marker.action = visualization_msgs::Marker::ADD;
-// %EndTag(ACTION)%
+    // Set the marker action.  Options are ADD, DELETE, and DELETEALL
+    marker.action = visualization_msgs::msg::Marker::ADD;
 
     // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-// %Tag(POSE)%
     marker.pose.position.x = 0;
     marker.pose.position.y = 0;
     marker.pose.position.z = 0;
@@ -82,63 +69,51 @@ int main( int argc, char** argv )
     marker.pose.orientation.y = 0.0;
     marker.pose.orientation.z = 0.0;
     marker.pose.orientation.w = 1.0;
-// %EndTag(POSE)%
 
     // Set the scale of the marker -- 1x1x1 here means 1m on a side
-// %Tag(SCALE)%
     marker.scale.x = 1.0;
     marker.scale.y = 1.0;
     marker.scale.z = 1.0;
-// %EndTag(SCALE)%
 
     // Set the color -- be sure to set alpha to something non-zero!
-// %Tag(COLOR)%
     marker.color.r = 0.0f;
     marker.color.g = 1.0f;
     marker.color.b = 0.0f;
     marker.color.a = 1.0;
-// %EndTag(COLOR)%
 
-// %Tag(LIFETIME)%
-    marker.lifetime = ros::Duration();
-// %EndTag(LIFETIME)%
+    // Set the lifetime of the marker -- 0 indicates forever
+    marker.lifetime = rclcpp::Duration::from_nanoseconds(0);
 
     // Publish the marker
-// %Tag(PUBLISH)%
-    while (marker_pub.getNumSubscribers() < 1)
+    while (marker_pub->get_subscription_count() < 1)
     {
-      if (!ros::ok())
+      if (!rclcpp::ok())
       {
         return 0;
       }
-      ROS_WARN_ONCE("Please create a subscriber to the marker");
-      ros::Duration(1.0).sleep();
+      auto logger = rclcpp::get_logger("my_subscriber");
+      RCLCPP_WARN(logger, "Please create a subscriber to the marker");
+      loop_rate.sleep();
     }
-    marker_pub.publish(marker);
-// %EndTag(PUBLISH)%
+    marker_pub->publish(marker);
 
     // Cycle between different shapes
-// %Tag(CYCLE_SHAPES)%
     switch (shape)
     {
-    case visualization_msgs::Marker::CUBE:
-      shape = visualization_msgs::Marker::SPHERE;
+    case visualization_msgs::msg::Marker::CUBE:
+      shape = visualization_msgs::msg::Marker::SPHERE;
       break;
-    case visualization_msgs::Marker::SPHERE:
-      shape = visualization_msgs::Marker::ARROW;
+    case visualization_msgs::msg::Marker::SPHERE:
+      shape = visualization_msgs::msg::Marker::ARROW;
       break;
-    case visualization_msgs::Marker::ARROW:
-      shape = visualization_msgs::Marker::CYLINDER;
+    case visualization_msgs::msg::Marker::ARROW:
+      shape = visualization_msgs::msg::Marker::CYLINDER;
       break;
-    case visualization_msgs::Marker::CYLINDER:
-      shape = visualization_msgs::Marker::CUBE;
+    case visualization_msgs::msg::Marker::CYLINDER:
+      shape = visualization_msgs::msg::Marker::CUBE;
       break;
     }
-// %EndTag(CYCLE_SHAPES)%
 
-// %Tag(SLEEP_END)%
-    r.sleep();
+    loop_rate.sleep();
   }
-// %EndTag(SLEEP_END)%
 }
-// %EndTag(FULLTEXT)%
