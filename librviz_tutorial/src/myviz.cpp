8dd3fe3591d2e14b82ad6652c691136ce2fb03a9
Rebecca Butler <rebecca@openrobotics.org>
2021-08-06 16:29:11 -0400

    Fix linter errors
    Signed-off-by: Rebecca Butler <rebecca@openrobotics.org>

--- librviz_tutorial/src/myviz.cpp
+++ librviz_tutorial/src/myviz.cpp
@@ -1,31 +1,31 @@
-/*
- * Copyright (c) 2012, Willow Garage, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Willow Garage, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+// Copyright (c) 2011, Willow Garage, Inc.
+// All rights reserved.
+//
+// Software License Agreement (BSD License 2.0)
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Willow Garage, Inc. nor the names of its
+//       contributors may be used to endorse or promote products derived from
+//       this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
 #include "myviz.hpp"
 
@@ -35,6 +35,8 @@
 #include <QGridLayout>
 #include <QVBoxLayout>
 
+#include <memory>
+
 #include "rviz_common/display.hpp"
 #include "rviz_common/render_panel.hpp"
 #include "rviz_common/visualization_manager.hpp"
@@ -42,36 +44,36 @@
 
 // BEGIN_TUTORIAL
 // Constructor for MyViz.  This does most of the work of the class.
-MyViz::MyViz( QWidget* parent )
-  : QWidget( parent )
+MyViz::MyViz(QWidget * parent)
+: QWidget(parent)
 {
   // Construct and lay out labels and slider controls.
-  QLabel* thickness_label = new QLabel( "Line Thickness" );
-  QSlider* thickness_slider = new QSlider( Qt::Horizontal );
-  thickness_slider->setMinimum( 1 );
-  thickness_slider->setMaximum( 100 );
-  QLabel* cell_size_label = new QLabel( "Cell Size" );
-  QSlider* cell_size_slider = new QSlider( Qt::Horizontal );
-  cell_size_slider->setMinimum( 1 );
-  cell_size_slider->setMaximum( 100 );
-  QGridLayout* controls_layout = new QGridLayout();
-  controls_layout->addWidget( thickness_label, 0, 0 );
-  controls_layout->addWidget( thickness_slider, 0, 1 );
-  controls_layout->addWidget( cell_size_label, 1, 0 );
-  controls_layout->addWidget( cell_size_slider, 1, 1 );
+  QLabel * thickness_label = new QLabel("Line Thickness");
+  QSlider * thickness_slider = new QSlider(Qt::Horizontal);
+  thickness_slider->setMinimum(1);
+  thickness_slider->setMaximum(100);
+  QLabel * cell_size_label = new QLabel("Cell Size");
+  QSlider * cell_size_slider = new QSlider(Qt::Horizontal);
+  cell_size_slider->setMinimum(1);
+  cell_size_slider->setMaximum(100);
+  QGridLayout * controls_layout = new QGridLayout();
+  controls_layout->addWidget(thickness_label, 0, 0);
+  controls_layout->addWidget(thickness_slider, 0, 1);
+  controls_layout->addWidget(cell_size_label, 1, 0);
+  controls_layout->addWidget(cell_size_slider, 1, 1);
 
   // Construct and lay out render panel.
   render_panel_ = new rviz_common::RenderPanel();
-  QVBoxLayout* main_layout = new QVBoxLayout;
-  main_layout->addLayout( controls_layout );
-  main_layout->addWidget( render_panel_ );
+  QVBoxLayout * main_layout = new QVBoxLayout;
+  main_layout->addLayout(controls_layout);
+  main_layout->addWidget(render_panel_);
 
   // Set the top-level layout for this MyViz widget.
-  setLayout( main_layout );
+  setLayout(main_layout);
 
   // Make signal/slot connections.
-  connect( thickness_slider, SIGNAL( valueChanged( int )), this, SLOT( setThickness( int )));
-  connect( cell_size_slider, SIGNAL( valueChanged( int )), this, SLOT( setCellSize( int )));
+  connect(thickness_slider, SIGNAL(valueChanged(int)), this, SLOT(setThickness(int)));
+  connect(cell_size_slider, SIGNAL(valueChanged(int)), this, SLOT(setCellSize(int)));
 
   // Next we initialize the main RViz classes.
   //
@@ -79,8 +81,9 @@ MyViz::MyViz( QWidget* parent )
   // holds the main Ogre scene, holds the ViewController, etc.  It is
   // very central and we will probably need one in every usage of
   // librviz.
-  auto rviz_ros_node = std::make_shared<rviz_common::ros_integration::RosNodeAbstraction>("rviz_node");
-  rviz_common::WindowManagerInterface* wm = nullptr;
+  auto rviz_ros_node = std::make_shared
+    <rviz_common::ros_integration::RosNodeAbstraction>("rviz_node");
+  rviz_common::WindowManagerInterface * wm = nullptr;
   auto clock = rviz_ros_node->get_raw_node()->get_clock();
   manager_ = new rviz_common::VisualizationManager(render_panel_, rviz_ros_node, wm, clock);
 
@@ -90,18 +93,18 @@ MyViz::MyViz( QWidget* parent )
   manager_->startUpdate();
 
   // Create a Grid display.
-  grid_ = manager_->createDisplay( "rviz/Grid", "adjustable grid", true );
+  grid_ = manager_->createDisplay("rviz/Grid", "adjustable grid", true);
   if (grid_ == NULL) {
     RCLCPP_ERROR(rclcpp::get_logger("myviz"), "Error creating grid display");
   }
 
   // Configure the GridDisplay the way we like it.
-  grid_->subProp( "Line Style" )->setValue( "Billboards" );
-  grid_->subProp( "Color" )->setValue( QColor( Qt::yellow ) );
+  grid_->subProp("Line Style")->setValue("Billboards");
+  grid_->subProp("Color")->setValue(QColor(Qt::yellow));
 
   // Initialize the slider values.
-  thickness_slider->setValue( 25 );
-  cell_size_slider->setValue( 10 );
+  thickness_slider->setValue(25);
+  cell_size_slider->setValue(10);
 }
 
 // Destructor.
@@ -113,21 +116,19 @@ MyViz::~MyViz()
 // This function is a Qt slot connected to a QSlider's valueChanged()
 // signal.  It sets the line thickness of the grid by changing the
 // grid's "Line Width" property.
-void MyViz::setThickness( int thickness_percent )
+void MyViz::setThickness(int thickness_percent)
 {
-  if( grid_ != NULL )
-  {
-    grid_->subProp( "Line Style" )->subProp( "Line Width" )->setValue( thickness_percent / 100.0f );
+  if (grid_ != NULL) {
+    grid_->subProp("Line Style")->subProp("Line Width")->setValue(thickness_percent / 100.0f);
   }
 }
 
 // This function is a Qt slot connected to a QSlider's valueChanged()
 // signal.  It sets the cell size of the grid by changing the grid's
 // "Cell Size" Property.
-void MyViz::setCellSize( int cell_size_percent )
+void MyViz::setCellSize(int cell_size_percent)
 {
-  if( grid_ != NULL )
-  {
-    grid_->subProp( "Cell Size" )->setValue( cell_size_percent / 10.0f );
+  if (grid_ != NULL) {
+    grid_->subProp("Cell Size")->setValue(cell_size_percent / 10.0f);
   }
 }
--- librviz_tutorial/src/myviz.hpp
+++ librviz_tutorial/src/myviz.hpp
@@ -1,31 +1,32 @@
-/*
- * Copyright (c) 2012, Willow Garage, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Willow Garage, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+// Copyright (c) 2011, Willow Garage, Inc.
+// All rights reserved.
+//
+// Software License Agreement (BSD License 2.0)
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Willow Garage, Inc. nor the names of its
+//       contributors may be used to endorse or promote products derived from
+//       this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 #ifndef MYVIZ_HPP_
 #define MYVIZ_HPP_
 
@@ -40,21 +41,22 @@ class VisualizationManager;
 
 // BEGIN_TUTORIAL
 // Class "MyViz" implements the top level widget for this example.
-class MyViz: public QWidget
+class MyViz : public QWidget
 {
-Q_OBJECT
+  Q_OBJECT
+
 public:
-  MyViz( QWidget* parent = 0 );
+  explicit MyViz(QWidget * parent = 0);
   virtual ~MyViz();
 
 private Q_SLOTS:
-  void setThickness( int thickness_percent );
-  void setCellSize( int cell_size_percent );
+  void setThickness(int thickness_percent);
+  void setCellSize(int cell_size_percent);
 
 private:
-  rviz_common::VisualizationManager* manager_;
-  rviz_common::RenderPanel* render_panel_;
-  rviz_common::Display* grid_;
+  rviz_common::VisualizationManager * manager_;
+  rviz_common::RenderPanel * render_panel_;
+  rviz_common::Display * grid_;
 };
 // END_TUTORIAL
-#endif // MYVIZ_HPP_
+#endif  // MYVIZ_HPP_
